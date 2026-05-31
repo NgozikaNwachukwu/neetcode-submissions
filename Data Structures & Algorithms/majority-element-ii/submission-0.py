1191b46a53ea91540582f585174f6e61ceea8fab
Ngozika Nwachukwu <139152775+NgozikaNwachukwu@users.noreply.github.com>
2026-05-30 20:23:48 -0400

    Add: majority-element-ii - submission-0

---
+++ Data Structures & Algorithms/majority-element-ii/submission-0.py
@@ -0,0 +1,21 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        target = n//3
+        final = []
+        my_dict = {}
+
+        for num in nums:
+            if num in my_dict:
+                my_dict[num] += 1
+            else:
+                my_dict[num] = 1
+        for i, count in my_dict.items():
+            if count > target:
+                final.append(i)
+
+        return final
+         
+
+        
+        
